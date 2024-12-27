ec678b448f5f7487c1acb8abafc004460e959b77
rucharajmane <rucharajmane18@gmail.com>
2024-12-27 11:42:06 +0530

    .

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -19,7 +19,7 @@ export default function AddTopic() {
     }
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/api/topics`, {
+      const res = await fetch("http://localhost:3000/api/topics", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
